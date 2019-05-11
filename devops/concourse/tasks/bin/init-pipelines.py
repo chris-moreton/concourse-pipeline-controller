64557fe0cae1fc15069ebe416da8ea25db714797
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-11 12:53:21 +0100

    Put deploy key in file

--- devops/concourse/tasks/bin/init-pipelines.py
+++ devops/concourse/tasks/bin/init-pipelines.py
@@ -6,4 +6,6 @@ f = open(sys.argv[1])
 yaml_file = yaml.safe_load(f)
 for repo in yaml_file["repos"]:
     dir=os.path.dirname(os.path.realpath(__file__))
-    os.system("sh " + dir + "/clone.sh " + repo["name"] + " " + repo["deploy_key"])
+    f = open("deploy.key","w")
+    f.write(repo["deploy_key"])
+    os.system("sh " + dir + "/clone.sh " + repo["name"])
