feba92056a028edeb57aac20091c7717ca98c2bd
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-12 20:40:31 +0100

    More logging

--- devops/concourse/tasks/bin/clone.sh
+++ devops/concourse/tasks/bin/clone.sh
@@ -2,9 +2,6 @@
 GITHUB_REPO=$1
 DEPLOY_KEY_CREDHUB_LOCATION=$2
 
-mkdir ~/.ssh
-echo "credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION"
-
 echo "Loading environment from .env file"
 source .env
 
@@ -14,6 +11,8 @@ fly --target netsensia-concourse login --insecure --concourse-url https://concou
 echo "Logging into CredHub..."
 eval "$(control-tower info --iaas aws --env --region eu-west-2 netsensia-concourse)"
 
+echo "Getting deploy key from CredHub..."
+mkdir ~/.ssh
 credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION -k private_key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 git clone $1
