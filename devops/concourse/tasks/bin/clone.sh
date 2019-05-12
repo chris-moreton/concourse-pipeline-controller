fe7a0e2ffdb0186571fb6bc279b25e390e2c6857
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-12 20:48:27 +0100

    Disable checking for GitHub

--- devops/concourse/tasks/bin/clone.sh
+++ devops/concourse/tasks/bin/clone.sh
@@ -15,5 +15,6 @@ echo "Getting deploy key from CredHub..."
 mkdir ~/.ssh
 credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION -k private_key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-git clone -q $1
+ssh -o "StrictHostKeyChecking=no" git@github.com
+git clone  $1
 
