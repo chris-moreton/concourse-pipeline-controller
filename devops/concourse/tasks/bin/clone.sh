94918265da7b1e855287fc0eb4f3aafb0a636f4a
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-12 14:27:50 +0100

    Attempt to get deploy key for each repo

--- devops/concourse/tasks/bin/clone.sh
+++ devops/concourse/tasks/bin/clone.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+GITHUB_REPO=$1
+DEPLOY_KEY_CREDHUB_LOCATION=$2
+export CONCOURSE_NETSENSIA_PASSWORD=$3
+export AWS_ACCESS_KEY_ID=$4
+export AWS_SECRET_ACCESS_KEY=$5
+
+fly --target netsensia-concourse login --insecure --concourse-url https://concourse.netsensia.com -u admin -p $CONCOURSE_NETSENSIA_PASSWORD
+eval "$(control-tower info --iaas aws --env --region eu-west-2 netsensia-concourse)"
+
 mkdir ~/.ssh
-cat deploy.key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa
+credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION.private_key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 git clone $1 -key ~/.ssh/id_rsa
+
--- devops/concourse/tasks/bin/init-pipelines.py
+++ devops/concourse/tasks/bin/init-pipelines.py
@@ -3,9 +3,13 @@ import sys
 import os
 
 f = open(sys.argv[1])
+aws_id = sys.argv[2]
+aws_key = sys.argv[3]
+concourse_admin_password = sys.argv[4]
+
 yaml_file = yaml.safe_load(f)
 for repo in yaml_file["repos"]:
     dir=os.path.dirname(os.path.realpath(__file__))
     f = open("deploy.key","w")
-    f.write(repo["deploy_key"])
-    os.system("sh " + dir + "/clone.sh " + repo["name"])
+    f.write(repo["deploy_key_credhub_location"])
+    os.system("sh " + dir + "/clone.sh " + repo["name"] + " " + repo["deploy_key_credhub_location"] + " " + aws_id + " " + aws_key + " " + concourse_admin_password)
