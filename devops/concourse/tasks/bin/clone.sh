112037749c6f90e301e169b03415658311947959
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-12 20:12:56 +0100

    Login to CredHub everytime

--- devops/concourse/tasks/bin/clone.sh
+++ devops/concourse/tasks/bin/clone.sh
@@ -4,6 +4,16 @@ DEPLOY_KEY_CREDHUB_LOCATION=$2
 
 mkdir ~/.ssh
 echo "credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION"
+
+echo "Loading environment from .env file"
+source .env
+
+echo "Logging into Concourse..."
+fly --target netsensia-concourse login --insecure --concourse-url https://concourse.netsensia.com -u admin -p $CONCOURSE_NETSENSIA_PASSWORD
+
+echo "Logging into CredHub..."
+eval "$(control-tower info --iaas aws --env --region eu-west-2 netsensia-concourse)"
+
 credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION -k private_key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/deploy_key_tmp
 chmod 600 ~/.ssh/deploy_key_tmp
 ssh-add ~/.ssh/deploy_key_tmp
--- devops/concourse/tasks/bin/setenv.sh
+++ devops/concourse/tasks/bin/setenv.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-export AWS_ACCESS_KEY_ID=$1
-export AWS_SECRET_ACCESS_KEY=$2
-export CONCOURSE_NETSENSIA_PASSWORD=$3
+echo "AWS_ACCESS_KEY_ID=$1" > .env
+echo "AWS_SECRET_ACCESS_KEY=$2" >> .env
+echo "CONCOURSE_NETSENSIA_PASSWORD=$3" >> .env
 
-echo "Logging into Concourse..."
-fly --target netsensia-concourse login --insecure --concourse-url https://concourse.netsensia.com -u admin -p $CONCOURSE_NETSENSIA_PASSWORD
-
-echo "Logging into CredHub..."
-eval "$(control-tower info --iaas aws --env --region eu-west-2 netsensia-concourse)"
 
