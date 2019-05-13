9e58d3972bc40785b80fa4d34f7b69d2c46310b0
Chris Moreton <christopher.moreton@hmcts.net>
2019-05-13 11:08:42 +0100

    Use 1.0.4 docker image

--- devops/concourse/tasks/bin/clone.sh
+++ devops/concourse/tasks/bin/clone.sh
@@ -9,7 +9,6 @@ echo "Logging into CredHub..."
 eval "$(control-tower info --iaas aws --env --region eu-west-2 netsensia-concourse)"
 
 echo "Getting deploy key from CredHub..."
-mkdir ~/.ssh
 credhub get -q -n $DEPLOY_KEY_CREDHUB_LOCATION -k private_key | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh -o "StrictHostKeyChecking=no" git@github.com
