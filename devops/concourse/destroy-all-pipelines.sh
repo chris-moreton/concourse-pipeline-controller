e1ed1449822f13b9ce1b6c12a632b1a4e170239f
Chris Moreton <chris@netsensia.com>
2019-05-23 16:34:01 +0100

    Add destroy all pipelines script

---
+++ devops/concourse/destroy-all-pipelines.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+fly --target $CONCOURSE_NAME login --insecure --concourse-url $CONCOURSE_SERVER -u admin -p $CONCOURSE_ADMIN_PASSWORD
+sudo fly --target $CONCOURSE_NAME sync
+fly --target $CONCOURSE_NAME destroy-pipeline -p directorzone-api
+fly --target $CONCOURSE_NAME destroy-pipeline -p directorzone-api-shared-infra
+fly --target $CONCOURSE_NAME destroy-pipeline -p pipeline-controller
