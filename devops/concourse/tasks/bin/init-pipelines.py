ccded49e5d0e9b5ee86bebdaa1b0192dadce836a
Chris Moreton <chris@netsensia.com>
2019-05-26 13:03:21 +0100

    Remove global variables

--- devops/concourse/tasks/bin/init-pipelines.py
+++ devops/concourse/tasks/bin/init-pipelines.py
@@ -30,7 +30,7 @@ def get_state_repo_revisions():
     state_repo_revisions = {}
     for state_repo in state_yaml_file["repos"]:
         previous_head_revision = ""
-        if ("head_revision" in state_repo.keys()):
+        if "head_revision" in state_repo.keys():
             previous_head_revision = state_repo["head_revision"]
         state_repo_revisions[state_repo["pipeline_name"]] = previous_head_revision
     return state_repo_revisions
@@ -53,9 +53,11 @@ def system_call(call_string):
         exit(1)
 
 
-def initialise_pipeline(filename, directory_name, pipeline_name, first_job):
+def initialise_pipeline(repo):
+    pipeline_name = repo["pipeline_name"]
+    filename = "pipeline.yml"
     print("Looking for " + filename + "...")
-    pipeline_config = "/tmp/" + directory_name + "/devops/concourse/" + filename
+    pipeline_config = "/tmp/" + pipeline_name + "/devops/concourse/" + filename
     if os.path.isfile(pipeline_config):
         print("Updating pipeline " + pipeline_name + "...")
         system_call(
@@ -64,47 +66,46 @@ def initialise_pipeline(filename, directory_name, pipeline_name, first_job):
         print("Unpausing pipeline...")
         system_call("fly --target netsensia-concourse unpause-pipeline -p " + pipeline_name)
         print("Triggering build job...")
-        system_call("fly --target netsensia-concourse trigger-job -j " + pipeline_name + "/" + first_job)
+        system_call("fly --target netsensia-concourse trigger-job -j " + pipeline_name + "/" + "build")
     else:
         print("No " + filename + " found.")
 
 
 def get_deploy_key():
     print("Getting deploy key from CredHub...")
-    if ("deploy_key_credhub_location" in repo.keys()):
-        if ('PYCHARM_HOSTED' in os.environ.keys() and os.environ['PYCHARM_HOSTED'] == "1"):
-            deploy_key_file = "/tmp/id_rsa"
-        else:
-            deploy_key_file = "/root/.ssh/id_rsa"
 
-        prepare_deploy_key(deploy_key_file)
+    if 'PYCHARM_HOSTED' in os.environ.keys() and os.environ['PYCHARM_HOSTED'] == "1":
+        deploy_key_file = "/tmp/id_rsa"
+    else:
+        deploy_key_file = "/root/.ssh/id_rsa"
+
+    prepare_deploy_key(deploy_key_file)
 
 
-def prepare_deploy_key(deploy_key_file):
+def prepare_deploy_key(deploy_key_file, repo):
     print("Overwriting deploy key at " + deploy_key_file)
     sed = "sed -e 's/\(KEY-----\)\s/\\1\\n/g; s/\s\(-----END\)/\\n\\1/g' | sed -e '2s/\s\+/\\n/g'"
-    system_call("credhub get -q -n " + repo[
-        "deploy_key_credhub_location"] + " -k private_key | " + sed + " > " + deploy_key_file)
+    system_call("credhub get -q -n " + "/concourse/main/" + repo["pipeline_name"] + "/GITHUB_DEPLOY_KEY" + " -k private_key | " + sed + " > " + deploy_key_file)
     system_call("chmod 600 ~/.ssh/id_rsa")
 
 
-def clone_repository():
+def clone_repository(repo):
     os.system("ssh -o \"StrictHostKeyChecking=no\" git@github.com")
     system_call("rm -rf /tmp/" + repo["pipeline_name"])
     clone_dir = "/tmp/" + repo["pipeline_name"]
-    system_call("git clone " + repo["uri"] + " " + clone_dir)
+    system_call("git clone git@github.com:chris-moreton/" + repo["pipeline_name"] + " " + clone_dir)
     return clone_dir
 
 
-def get_current_head_revision():
+def get_current_head_revision(repo):
     get_deploy_key()
-    repo_object = git.Repo(clone_repository())
+    repo_object = git.Repo(clone_repository(repo["pipeline_name"]))
     return repo_object.head.commit.name_rev.split()[0]
 
 
-def get_previous_head_revision(pipeline_name):
-    state_repo_revisions = get_state_repo_revisions()
-    if pipeline_name in state_repo_revisions.keys():
+def get_previous_head_revision(repo):
+    state_repo_revisions = get_state_repo_revisions(repo)
+    if repo["pipeline_name"] in state_repo_revisions.keys():
         return state_repo_revisions[repo["pipeline_name"]]
     else:
         return ""
@@ -114,12 +115,12 @@ yaml_file = get_repos()
 
 for repo in yaml_file["repos"]:
 
-    current_head_revision = get_current_head_revision()
+    current_head_revision = get_current_head_revision(repo)
 
-    if current_head_revision == get_previous_head_revision(repo["pipeline_name"]):
+    if current_head_revision == get_previous_head_revision(repo):
         print("We've done this one before...")
     else:
-        initialise_pipeline('pipeline.yml', repo["pipeline_name"], repo["pipeline_name"], repo["first_job"])
+        initialise_pipeline(repo)
 
     repo["head_revision"] = current_head_revision
 
