a10e1152606a2c2ae070f958fb7ef9e449cb0956
Chris Moreton <chris@netsensia.com>
2019-05-27 17:00:16 +0100

    Another try

--- devops/terraform/product/data.tf
+++ devops/terraform/product/data.tf
@@ -6,6 +6,6 @@ data "credhub_value" "cloudfoundry_org_owner_guid" {
   name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_DEPLOY_USER_GUID"
 }
 
-data "credhub_value" "org_netsensia" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_ORGANISATION_NAME"
+data "credhub_value" "org_netsensia_guid" {
+  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_ORGANISATION_GUID"
 }
