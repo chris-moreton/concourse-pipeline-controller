23fa85a95f0aac4505d224fbe19989cb3632b866
Chris Moreton <chris@netsensia.com>
2019-05-27 21:59:20 +0100

    Make CF space resource identical for import

--- devops/terraform/component/resources.tf
+++ devops/terraform/component/resources.tf
@@ -1,6 +1,6 @@
 resource "cloudfoundry_space" "product_space" {
   name = "${var.product}-${var.environment}"
-  org = "${data.credhub_value.org_netsensia.value}"
+  org = "${data.credhub_value.org_netsensia_guid.value}"
   managers = [
     "${data.credhub_value.cloudfoundry_org_owner_guid.value}"
   ]
