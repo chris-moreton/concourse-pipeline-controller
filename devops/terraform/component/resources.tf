ce364b3d4dfee784120d203a71c170b23324afbd
Chris Moreton <chris@netsensia.com>
2019-05-31 11:36:05 +0100

    Put public bucket back

--- devops/terraform/component/resources.tf
+++ devops/terraform/component/resources.tf
@@ -20,3 +20,42 @@ resource "aws_s3_bucket" "private_bucket" {
   }
 }
 
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "${var.product}-${var.component}-${var.environment}-public"
+
+  region = "eu-west-2"
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_bucket" {
+  depends_on = ["aws_s3_bucket.public_bucket"]
+  bucket = "${aws_s3_bucket.public_bucket.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "Terraform",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+            "s3:GetObject"
+        ],
+        "Resource": [
+            "arn:aws:s3:::${var.product}-${var.component}-${var.environment}-public",
+            "arn:aws:s3:::${var.product}-${var.component}-${var.environment}-public/*"
+        ],
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "0.0.0.0/0"}
+      }
+    }
+  ]
+}
+POLICY
+}
+
