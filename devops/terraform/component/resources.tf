9fe89fc7bc05a27b83dcc5197f08a5539d87e6a8
Chris Moreton <chris@netsensia.com>
2019-05-31 11:34:05 +0100

    Delete public bucket

--- devops/terraform/component/resources.tf
+++ devops/terraform/component/resources.tf
@@ -20,33 +20,3 @@ resource "aws_s3_bucket" "private_bucket" {
   }
 }
 
-
-resource "aws_s3_bucket_policy" "public_bucket" {
-  depends_on = ["aws_s3_bucket.public_bucket"]
-  bucket = "${aws_s3_bucket.public_bucket.id}"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "Terraform",
-  "Statement": [
-    {
-      "Sid": "IPAllow",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
-            "s3:GetObject"
-        ],
-        "Resource": [
-            "arn:aws:s3:::${var.product}-${var.component}-${var.environment}-public",
-            "arn:aws:s3:::${var.product}-${var.component}-${var.environment}-public/*"
-        ],
-      "Condition": {
-         "IpAddress": {"aws:SourceIp": "0.0.0.0/0"}
-      }
-    }
-  ]
-}
-POLICY
-}
-
