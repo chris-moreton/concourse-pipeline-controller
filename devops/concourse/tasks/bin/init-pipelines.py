35f8e495516ebf8e31db776ee55a78b89fcf272e
Chris Moreton <chris@netsensia.com>
2019-06-27 18:30:06 +0100

    Reposition unsed

--- devops/concourse/tasks/bin/init-pipelines.py
+++ devops/concourse/tasks/bin/init-pipelines.py
@@ -99,6 +99,7 @@ def initialise_pipeline(repo):
             f.write(merged)
             f.close()
             merged_pipeline_config = merged_config
+            system_call("sed -i s/" + project_type + "/PROJECT_TYPE/g " + core_config)
         else:
             merged_pipeline_config = core_config
             print("No " + filename + " found.")
@@ -107,7 +108,6 @@ def initialise_pipeline(repo):
     system_call(
         "fly --target netsensia-concourse set-pipeline --non-interactive -c " + merged_pipeline_config + " -p " + pipeline_name
     )
-    system_call("sed -i s/" + project_type + "/PROJECT_TYPE/g " + core_config)
     print("Unpausing pipeline...")
     system_call("fly --target netsensia-concourse unpause-pipeline -p " + pipeline_name)
 
