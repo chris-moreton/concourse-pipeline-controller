0e677afc58e58fe2476d8199954aede5e591a084
Chris Moreton <chris@netsensia.com>
2019-07-05 14:48:13 +0100

    Relocation of pipeline vars

--- devops/terraform/component/data.tf
+++ devops/terraform/component/data.tf
@@ -1,9 +1,9 @@
 data "credhub_value" "aws_terraform_access_key_id" {
-  name = "/concourse/main/pipeline-controller/AWS_ACCESS_KEY_ID"
+  name = "/concourse/pipeline/controller/AWS_ACCESS_KEY_ID"
 }
 
 data "credhub_value" "aws_terraform_secret_access_key" {
-  name = "/concourse/main/pipeline-controller/AWS_SECRET_ACCESS_KEY"
+  name = "/concourse/pipeline/controller/AWS_SECRET_ACCESS_KEY"
 }
 
 data "aws_vpc" "default" {
@@ -19,26 +19,26 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "credhub_user" "cloudfoundry_org_owner" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_DEPLOY_USER"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_DEPLOY_USER"
 }
 
 data "credhub_value" "cloudfoundry_org_owner_guid" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_DEPLOY_USER_GUID"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_DEPLOY_USER_GUID"
 }
 
 data "credhub_value" "org_netsensia_guid" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_ORGANISATION_GUID"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_ORGANISATION_GUID"
 }
 
 data "credhub_user" "cloudfoundry_ibm_org_owner" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_IBM_DEPLOY_USER"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_IBM_DEPLOY_USER"
 }
 
 data "credhub_value" "cloudfoundry_ibm_org_owner_guid" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_IBM_DEPLOY_USER_GUID"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_IBM_DEPLOY_USER_GUID"
 }
 
 data "credhub_value" "org_ibm_netsensia_guid" {
-  name = "/concourse/main/pipeline-controller/CLOUD_FOUNDRY_IBM_ORGANISATION_GUID"
+  name = "/concourse/pipeline/controller/CLOUD_FOUNDRY_IBM_ORGANISATION_GUID"
 }
 
